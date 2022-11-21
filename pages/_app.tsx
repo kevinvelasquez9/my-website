e473b50eb7e4135d37bf3e2075cc86b68c7614c0
Kevin Velasquez <60550789+kevinvelasquez9@users.noreply.github.com>
2022-11-20 19:30:05 -0500

    added description to metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,16 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Kevin Velasquez</title>
+        <meta
+          name="description"
+          content="Personal website of Kevin Velasquez, a student at Johns Hopkins University. Previous intern at Google and incoming Software Engineer at Netflix."
+          key="desc"
+        />
+        <meta
+          property="og:description"
+          content="Personal website of Kevin Velasquez"
+          key="ogdesc"
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
